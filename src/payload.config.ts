ea4bf1206035992c4b606e23e5a2d388f516c564
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-01-08 00:02:24 +0100

    Application architecture modeling

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,3 +1,4 @@
+import { Media } from './collections/Media';
 import dotenv from 'dotenv';
 import { buildConfig } from "payload/config"
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
@@ -5,6 +6,9 @@ import {slateEditor } from "@payloadcms/richtext-slate"
 import {webpackBundler} from "@payloadcms/bundler-webpack"
 import path from "path"
 import { Users } from "./collections/Users"
+import { Products } from './collections/Products/Products';
+import { ProductFiles } from './collections/ProductFile';
+import { Orders } from './collections/Orders';
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-    collections: [Users],
+    collections: [Users, Products, Media, ProductFiles, Orders],
     routes: {
         admin: '/sell',
     },
