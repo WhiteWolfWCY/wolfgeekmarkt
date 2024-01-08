e6cfa56f8a8f9b540afb8fd35fdb1b66715eaf88
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-01-08 14:45:21 +0100

    Products fetch and product swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { buttonVariants } from "@/components/ui/button";
 import { Button } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
@@ -8,19 +9,22 @@ const perks = [
   {
     name: "Instant Delivery",
     icon: ArrowDownToLine,
-    description: "Get your assets delivered to your email in seconds, and download them right away!"
+    description:
+      "Get your assets delivered to your email in seconds, and download them right away!",
   },
   {
     name: "Guaranteed Quality",
     icon: CheckCircle,
-    description: "Every asset on our platform is verified by our team to ensure the highest quality!"
+    description:
+      "Every asset on our platform is verified by our team to ensure the highest quality!",
   },
   {
     name: "For The Planet",
     icon: Leaf,
-    description: "We've pledged 1% of sales to the preservation and restoration of the natural environment!"
+    description:
+      "We've pledged 1% of sales to the preservation and restoration of the natural environment!",
   },
-]
+];
 
 export default function Home() {
   return (
@@ -32,8 +36,8 @@ export default function Home() {
             <span className="text-violet-600">digital assets</span>.
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-            Welcome to WolfGeekmarkt. Every asset on our platform is verified
-            by us to ensure the best quality.
+            Welcome to WolfGeekmarkt. Every asset on our platform is verified by
+            us to ensure the best quality.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 mt-6">
             <Link href="/products" className={buttonVariants()}>
@@ -43,16 +47,23 @@ export default function Home() {
           </div>
         </div>
 
-        {/* TODO: List of products */}
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          title="Brand new"
+          href="/products"
+        />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
           <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
-            {perks.map((perk)=>(
-              <div key={perk.name} className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+              >
                 <div className="md:flex-shrink-0 flex justify-center">
                   <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
-                    {<perk.icon className="w-1/3 h-1/3"/>}
+                    {<perk.icon className="w-1/3 h-1/3" />}
                   </div>
                 </div>
                 <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
